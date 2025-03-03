519992ef1d717f99e0667c851de9547aa18bb5dc
cmames <c.mames@free.fr>
2025-03-03 16:30:08 +0100

    hotfix esnext

--- dist/coreroute.js
+++ dist/coreroute.js
@@ -1,43 +1,7 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CoreRoute = void 0;
-const http = __importStar(require("http"));
-const https = __importStar(require("https"));
-const fs = __importStar(require("fs"));
-const path = __importStar(require("path"));
+import * as http from 'http';
+import * as https from 'https';
+import * as fs from "fs";
+import * as path from "path";
 /**
  * @author Mames Christophe
  * @license GPL-3.0-or-later
@@ -49,7 +13,7 @@ const path = __importStar(require("path"));
  * to full-fledged frameworks like ExpressJS for simpler projects.<br>
  *<br>
  */
-class CoreRoute {
+export class CoreRoute {
     #gets;
     #puts;
     #posts;
@@ -367,4 +331,3 @@ class CoreRoute {
         res.end(message);
     }
 }
-exports.CoreRoute = CoreRoute;
